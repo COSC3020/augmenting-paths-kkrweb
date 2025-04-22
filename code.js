629fd354fa86a3426e28643272809c5154753dfc
Kane Kriz <kanemkriz@gmail.com>
2025-04-21 19:12:30 -0600

    Update code.js

--- code.js
+++ code.js
@@ -8,12 +8,72 @@
 //
 
 
-function augmentingPath(graph, start, end)
+
+function augmentingPath(graph, startNode, endNode) 
 {
-    //
+    if(startNode == endNode) //early check, start and end are same node
+    {
+        return [startNode];
+    }
+
+    var visitedNodes = {};
+    var parentMap = {};
+    var nodeQueue = [startNode];
+    
+    var pathExists = false;
+    visitedNodes[startNode] = true;
+    
+
+    for(var index = 0; index < nodeQueue.length; index++) 
+    {
+        var currentNode = nodeQueue[index];
+        var neighborNodes;
+        
+        if((graph[currentNode] !== undefined) && (graph[currentNode] !== null))
+        {
+            neighborNodes = graph[currentNode];
+        } 
+            
+        else 
+        {
+            neighborNodes = {};
+        }
+
+        for(var neighborNode in neighborNodes) 
+        {
+            if((!visitedNodes[neighborNode]) && (neighborNodes[neighborNode] > 0)) 
+            {
+                parentMap[neighborNode] = currentNode;
+                visitedNodes[neighborNode] = true;
+                nodeQueue.push(neighborNode);
+
+                if(neighborNode == endNode) 
+                {
+                    pathExists = true;
+                    index = nodeQueue.length;
+                    break;
+                }
+            }
+        }
+    }
+
+    if(pathExists)
+    {
+        var pathNodes = [];
+        var pathNode = endNode;
+        
+        while(pathNode != startNode) 
+        {
+            pathNodes.unshift(pathNode);
+            pathNode = parentMap[pathNode];
+        }
+        
+        pathNodes.unshift(startNode);
+        return pathNodes;
+    }
+
     return [];
 }
 
 
-
-//helper? maybe
+//
